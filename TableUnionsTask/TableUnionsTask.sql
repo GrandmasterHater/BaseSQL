3e751239c35c1ae4e81da44bdacf3e0352bba8ca
sguschin <sguschin@gear-games.com>
2025-06-21 00:37:04 +0400

    Base SQL TableUnionsTask

---
+++ TableUnionsTask/TableUnionsTask.sql
@@ -0,0 +1,14 @@
+--8.3.1.
+SELECT Products.ProductName, Categories.CategoryID, Categories.CategoryName
+FROM northwind.dbo.Products as Products, northwind.dbo.Categories as Categories
+WHERE Products.CategoryID = Categories.CategoryID;
+
+--8.3.2.
+SELECT Products.ProductName, Products.UnitPrice
+FROM northwind.dbo.Products as Products, northwind.dbo.[Order Details] as OrderDetails
+WHERE Products.ProductID = OrderDetails.ProductID AND OrderDetails.UnitPrice < 20;
+
+--8.3.3.
+SELECT Products.ProductName, Products.UnitPrice, Categories.CategoryID, Categories.CategoryName
+FROM northwind.dbo.Products as Products, northwind.dbo.[Order Details] as OrderDetails, northwind.dbo.Categories as Categories
+WHERE Products.ProductID = OrderDetails.ProductID AND OrderDetails.UnitPrice < 20 AND Products.CategoryID = Categories.CategoryID;

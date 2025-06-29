013a573b94717b634c468684fef11c06fe8be258
sguschin <sguschin@gear-games.com>
2025-06-29 09:55:36 +0400

    Base SQL: Add indexes for descriptions on UniqueAndIndexesTask

--- UniqueAndIndexes.sql
+++ UniqueAndIndexes.sql
@@ -1,1 +1,6 @@
 CREATE INDEX IX_Territories_RegionID ON Territories (RegionID);
+
+--Предполагаю, что для случаев выборок по столбцам с описанием будет удобно их проиндексировать
+CREATE INDEX IX_Region_Description ON Region (RegionDescription);
+
+CREATE INDEX IX_Territories_TerritoryDescription ON Territories (TerritoryDescription);
